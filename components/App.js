44df8d03d8f875df706e898d2f5baf09e0b5d6e9
Riza Fahmi <rizafahmi@gmail.com>
2017-04-29 09:41:14 +0700

    Passing test initial state of age

--- components/App.js
+++ components/App.js
@@ -10,29 +10,39 @@ import styles from './App.css'
 
 const NoOp = () => {}
 
-const App = (props) => {
-  return (
-    <View style={styles.container}>
-      <StatusBar
-        backgroundColor='#29434e'
-        barStyle='light-content'
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      age: 0
+    }
+  }
+
+  render () {
+    return (
+      <View style={styles.container}>
+        <StatusBar
+          backgroundColor='#29434e'
+          barStyle='light-content'
    />
-      <View style={styles.header}>
-        <Text style={styles.headerText}>Mango Tree</Text>
-      </View>
-      <View style={styles.stats}>
-        <Text style={styles.statsText}>Age: </Text>
-        <Text style={styles.statsText}>0</Text>
-      </View>
-      <View style={styles.buttons}>
-        <Button
-          title='Simulate'
-          onPress={() => NoOp}
-          color='#546e7a'
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Mango Tree</Text>
+        </View>
+        <View style={styles.stats}>
+          <Text style={styles.statsText}>Age: </Text>
+          <Text style={styles.statsText}>0</Text>
+        </View>
+        <View style={styles.buttons}>
+          <Button
+            title='Simulate'
+            onPress={() => NoOp}
+            color='#546e7a'
       />
+        </View>
       </View>
-    </View>
-  )
+    )
+  }
 }
 
 export default App
