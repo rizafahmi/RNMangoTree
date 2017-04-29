939ac6ad062f4fd333bd6849eda636b3d6002433
Riza Fahmi <rizafahmi@gmail.com>
2017-04-29 08:03:15 +0700

    Test for header text

--- components/App.test.js
+++ components/App.test.js
@@ -4,9 +4,20 @@ import { shallow } from 'enzyme'
 import App from './App.js'
 
 describe('Renders <App />', () => {
+  let app
+  beforeEach(() => {
+    app = shallow(<App />)
+  })
   test('<App /> should render properly', () => {
-    const app = shallow(<App />)
     expect(app).toBeDefined()
     expect(app).toHaveLength(1)
+  })
+  describe('<App /> should have header text, stats text and simulate button', () => {
+    test('should render header text', () => {
+      expect(app.find('Text')).toHaveLength(1)
+      expect(app.find('Text').props().children).toEqual('Mango Tree')
+    })
+    test('should render stats text')
+    test('should render simulate button')
   })
 })
